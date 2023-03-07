5e4c2e2699e6ecf624b838c18a90b58303793261
XirTech <john.darlucio022@gmail.com>
2023-03-07 12:37:55 +0800

    fix dark mode in css

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,20 +13,38 @@ for (let i = 0; i < btns.length; i++) {
 
 // Dark mode query
 const toggle = document.getElementById("toggleDark");
-const body = document.querySelector("body");
+const body1 = document.querySelector("body");
 
 toggle.addEventListener("click", function () {
   this.classList.toggle("bi-moon-stars");
   if (this.classList.toggle("bi-moon-stars-fill")) {
-    body.style.background = "#fff";
-    body.style.color = "#000";
-    body.style.transition = "2s";
+    // body.style.background = "#fff";
+    // body.style.color = "#000";
+    // body.style.transition = "2s";
   } else {
-    body.style.background = "#121212";
-    body.style.color = "#fff";
-    body.style.transition = "2s";
+    // body.style.background = "#121212";
+    // body.style.color = "#fff";
+    // body.style.transition = "2s";
   }
 });
 
+// function change() {
+//   const get = document.querySelector(".lightMode");
+//   get.classList.toggle("darkMode")
+// }
+// document.querySelector("#toggleDark").addEventListener("click", change);
 
+
+const body = document.getElementById("changeStyle");
+function changeStyle() {
+  for (var i = 0; i < body.classList.length; i++) {
+    if (body.classList[i].indexOf("lightMode") >= 0) {
+      body.classList.remove("lightMode");
+      body.classList.add("darkMode");
+    } else {
+      body.classList.remove("darkMode");
+      body.classList.add("lightMode");
+    }
+  }
+}
 
