1456a52e7cc230fa86e92cddd15b81f36e05471e
XirTech <john.darlucio022@gmail.com>
2023-03-07 09:05:14 +0800

    fix header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,9 @@
-// Top nav 
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
- 
 // Add active class to the current button (highlight it)
-var header = document.getElementById("header");
-var btns = header.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
+let header = document.getElementById("header");
+let btns = header.getElementsByClassName("btn");
+for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function () {
-    var current = document.getElementsByClassName("active");
+    let current = document.getElementsByClassName("active");
     if (current.length > 0) {
       current[0].className = current[0].className.replace(" active", "");
     }
@@ -21,20 +11,22 @@ for (var i = 0; i < btns.length; i++) {
   });
 }
 
-// Embed an SVG for all <img> elements 
-  for (let img of document.querySelectorAll("img")) {
-    img.alt = "circle";
-    img.src =
-      "data:image/svg+xml;base64," +
-      btoa(`
-      <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-        <style>
-          :root { color: blue }
-          @media (prefers-color-scheme: dark) {
-            :root { color: purple }
-          }
-        </style>
-        <circle fill="currentColor" cx="16" cy="16" r="16"/>
-      </svg>
-    `);
+// Dark mode query
+const toggle = document.getElementById("toggleDark");
+const body = document.querySelector("body");
+
+toggle.addEventListener("click", function () {
+  this.classList.toggle("bi-moon-stars");
+  if (this.classList.toggle("bi-moon-stars-fill")) {
+    body.style.background = "white";
+    body.style.color = "black";
+    body.style.transition = "2s";
+  } else {
+    body.style.background = "black";
+    body.style.color = "white";
+    body.style.transition = "2s";
   }
+});
+
+
+
