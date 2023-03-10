2ff77e15611a0c2459f66ff9675da0a8d5ce0ca3
XirTech <john.darlucio022@gmail.com>
2023-03-11 07:02:02 +0800

    fix href

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+// Burger Dropdown button
+const toggleBtn = document.querySelector(".toggle_btn");
+const toggleBtnIcon = document.querySelector(".toggle_btn i");
+const dropDownMenu = document.querySelector(".dropdown_menu");
+
+toggleBtn.onclick = function () {
+  dropDownMenu.classList.toggle("open");
+  const isOpen = dropDownMenu.classList.contains("open");
+
+  toggleBtnIcon.classList = isOpen ? "bi bi-x" : "bi bi-list";
+};
+
+
+
+
 // Add active class to the current button (highlight it)
 let header = document.getElementById("header");
 let btns = header.getElementsByClassName("btn");
@@ -18,25 +33,11 @@ const body = document.querySelector("body");
 toggle.addEventListener("click", function () {
   this.classList.toggle("bi-moon-stars");
   if (this.classList.toggle("bi-moon-stars-fill")) {
-     body.classList.remove("darkMode");
+    body.classList.remove("darkMode");
     //  body.classList.add("lightMode");
   } else {
-      // body.classList.remove("lightMode");
-      body.classList.add("darkMode");
-      body.style.transition = '2s';
+    // body.classList.remove("lightMode");
+    body.classList.add("darkMode");
+    body.style.transition = "2s";
   }
 });
-
-// Burger Dropdown button 
-const toggleBtn = document.querySelector(".toggle_btn");
-const toggleBtnIcon = document.querySelector(".toggle_btn i");
-const dropDownMenu = document.querySelector(".dropdown_menu");
-
-toggleBtn.onclick = function () {
-  dropDownMenu.classList.toggle("open");
-  const isOpen = dropDownMenu.classList.contains("open");
-
-  toggleBtnIcon.classList = isOpen ? "bi bi-x" : "bi bi-list";
-};
-
-
